05eb37b286592060793acab6fb392fe368da26e4
sharath9620 <119046105+sharath9620@users.noreply.github.com>
2025-03-24 13:07:32 +0530

    Update app_mongo.py

--- app_mongo.py
+++ app_mongo.py
@@ -4,9 +4,14 @@ from pymongo import MongoClient
 class AppMongoDb:
     def __init__(self, config: FlaskConfig):
         self.connection_string = config["MONGODB_CONNECTION_STRING"]
+        self.mongodb_name = config["MONGODB_NAME"]
         self.client = MongoClient(self.connection_string)
 
     def get_collection(self, collection_name):
         # Example of accessing a specific database (replace 'mydatabase' with your DB name)
-        db = self.client["ApplicationHDev"]
+        db = self.client[self.mongodb_name]
         return db[collection_name]
+    
+    def list_collections(self):
+        db = self.client[self.mongodb_name]
+        return db.list_collection_names()
